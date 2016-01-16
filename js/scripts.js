40c8c3fa7f95c8379525eee66be2509965ff6d88
William Funk <contact@WilliamRADFunk.com>
2016-01-16 05:44:28 -0500

    Fleshed out the registration structure and style. Added a logout CTA and cleared the login fields.

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,34 @@ function login()
 	if(username !== "" && password !== "")
 	{
 		document.getElementById("login").style.display = "none";
+		document.getElementById("logout").style.display = "block";
 		document.getElementById("feeds").style.display = "block";
 		document.getElementsByTagName("nav")[0].style.display = "block";
 	}
 }
+function logout()
+{
+	document.getElementById("input_username").value = "";
+	document.getElementById("input_pwd").value = "";
+	document.getElementById("login").style.display = "block";
+	document.getElementById("logout").style.display = "none";
+	document.getElementById("feeds").style.display = "none";
+	document.getElementById("list-view").style.display = "none";
+	document.getElementById("profile").style.display = "none";
+	document.getElementsByTagName("nav")[0].style.display = "none";
+}
 function register()
 {
 	console.log("Registering");
+
+	document.getElementById("login").style.display = "none";
+	document.getElementById("register").style.display = "block";
+}
+function submitRegistration()
+{
+	document.getElementById("login").style.display = "none";
+	document.getElementById("feeds").style.display = "block";
+	document.getElementsByTagName("nav")[0].style.display = "block";
 }
 function seeConspiracy(consp)
 {
